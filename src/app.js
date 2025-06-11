c9adbb92a968edb98bebf5e7f1e5d6d5cb3f7629
aryan-git-007 <sahuaryan221@gmail.com>
2025-06-11 11:27:51 +0530

    attached new mongoose model files

--- src/app.js
+++ src/app.js
@@ -1,4 +1,19 @@
 import express from "express";
 const app = express()
 export {app}
+app.use(express.json({
+    limit:"10kb"
+}))
+app.use(express.urlencoded({extended:true,
+    limit:"12kb"
+}))
+app.use(express.static("public"))
 
+import cors from "cors";
+app.use(cors({
+   origin:process.env.CORS_ORIGIN,
+   credentials:true
+}))
+
+import cookieParser from "cookie-parser";
+app.use(cookieParser())
